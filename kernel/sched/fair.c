21442d986c1fc0ed859c8d7c7df36227d9d6da67
Chris Redpath <chris.redpath@arm.com>
2017-01-25 13:27:53 +0000

    sched: revert UTIL_EST usage from commit 6bf72ca7f1
    Change-Id: If13e2b2066efaa86cd1ec9082abbcba09a2bbe36
    Signed-off-by: Chris Redpath <chris.redpath@arm.com>

--- kernel/sched/fair.c
+++ kernel/sched/fair.c
@@ -4009,6 +4009,9 @@ static inline void hrtick_update(struct rq *rq)
 
 #ifdef CONFIG_SMP
 static bool cpu_overutilized(int cpu);
+static inline unsigned long boosted_cpu_util(int cpu);
+#else
+#define boosted_cpu_util(cpu) cpu_util(cpu)
 #endif
 
 #ifdef CONFIG_CPU_FREQ_GOV_SCHED
